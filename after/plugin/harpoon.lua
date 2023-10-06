e144b9ffbd65a08cb31d40ff1b71fea00b711d9e
george iliadis <george.iliadis.91@gmail.com>
2023-10-07 00:13:44 +0300

    Harpoon increased registers to 6

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,5 +8,7 @@ vim.keymap.set("n", "<leader>g1", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>g2", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<leader>g3", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<leader>g4", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>g5", function() ui.nav_file(5) end)
+vim.keymap.set("n", "<leader>g6", function() ui.nav_file(6) end)
 
 
