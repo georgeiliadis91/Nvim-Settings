b3ccb5e92676ed5e234b4ec1c22fd7f99b5f81a8
george iliadis <george.iliadis.91@gmail.com>
2023-10-28 00:59:29 +0300

    added relative path display for lua line

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,4 +2,20 @@ require('lualine').setup {
   options = {
     theme = 'material'
   }
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1, -- Use 1 to show relative path
+      },
+    },
+  },
 }
+
+-- Function to get the relative path
+local function get_relative_path()
+  local current_file = vim.fn.expand('%:p')
+  local project_root = vim.fn.finddir('your_project_root', '.;')
+  local relative_path = vim.fn.fnamemodify(current_file, ':~:.')
+  return vim.fn.pathshorten(vim.fn.fnamemodify(relative_path, ':~:' .. project_root))
+end
