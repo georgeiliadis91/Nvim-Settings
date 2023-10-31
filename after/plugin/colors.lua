b7ad599390cabad5571ea6d39a99b97d703553d7
george iliadis <george.iliadis.91@gmail.com>
2023-10-31 21:55:13 +0200

    fixed theme issues

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -64,10 +64,18 @@
 --     custom_highlights = {}, -- Overwrite highlights with your own
 -- })
 -- 
-require('lualine').setup({
-  options = {
-    theme = 'ayu',
-  },
+require('ayu').setup({
+   overrides = {
+        Normal = { bg = "None" },
+        ColorColumn = { bg = "None" },
+        SignColumn = { bg = "None" },
+        Folded = { bg = "None" },
+        FoldColumn = { bg = "None" },
+        CursorLine = { bg = "None" },
+        CursorColumn = { bg = "None" },
+        WhichKeyFloat = { bg = "None" },
+        VertSplit = { bg = "None" },
+    },
 })
 
  function ThemeUpdate(color)
